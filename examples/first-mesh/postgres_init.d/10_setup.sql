25ab8e6c796aace601463f5dea0c8e690eff977c
Ben LaRoque <benjamin.laroque@gmail.com>
2020-02-25 15:48:21 -0800

    adding logging to the first-mesh guide

--- examples/first-mesh/postgres_init.d/10_setup.sql
+++ examples/first-mesh/postgres_init.d/10_setup.sql
@@ -7,8 +7,10 @@ CREATE DATABASE sensor_data WITH TEMPLATE = template0;
 \connect sensor_data
 
 -- table for storing doubles
--- NOTE: this design is very bad and only used as an example for easy demonstration
---       a production system probably wants to map sensor names onto IDs, may want to allow annotations of entries, may want annotation of sensors, etc.
+-- NOTE: this design is very bad and only used as an example for easy
+--       demonstration a production system probably wants to map sensor names
+--       onto IDs may want to allow annotations of entries, may want annotation
+--       of sensors, etc.
 
 DROP TABLE IF EXISTS numeric_data;
 CREATE TABLE numeric_data (
